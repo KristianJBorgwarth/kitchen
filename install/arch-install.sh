1d61c2019f06578a8a982867a2f84fe5a58f81cf
Kristian JBF <10348902@pm.me>
2026-08-15 12:43:28 +0200

    feat: added waybar modules and updated installs
    waybar: added modules and updated some colors for thresholds
    install: added info outputs

--- install/arch-install.sh
+++ install/arch-install.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
+# tiling applications
+echo "==> Installing sway"
 sudo pacman -S sway
+echo "==> Installing waybar"
 sudo pacman -S waybar
+echo "==> Installing rofi"
 sudo pacman -S rofi
-sudo pacman -S python-pipx
+echo "==> Installing mako-notifier"
 sudo pacman -S mako-notifier
+
+## audio/video
+echo "==> Installing pipewire"
+sudo pacman -S pipewire
+echo "==> Installing pipewire-pulse"
+sudo pacman -S pipewire-pulse
+echo "==> Installing pipewire-alsa"
+sudo pacman -S pipewire-alsa
+echo "==> Installing wireplumber"
+sudo pacman -S wireplumber
+
+# pkg manager
+echo "==> Installing python-pipx"
+sudo pacman -S python-pipx
+
+# tools
+echo "==> Installing cmake"
 sudo pacman -S cmake
+echo "==> Installing ninja-build"
 sudo pacman -S ninja-build
+echo "==> Installing unzip"
+sudo pacman -S unzip
+echo "==> Installing brightnessctl"
+sudo pacman -S brightnessctl
+
+# dotnet
+echo "==> Installing dotnet-sdk-10.0"
 sudo pacman -S dotnet-sdk-10.0
--- install/ubuntu-install.sh
+++ install/ubuntu-install.sh
@@ -1,18 +1,33 @@
 
 # tiling applications
+echo "==> Installing waybar"
 sudo apt install -y waybar
+echo "==> Installing rofi"
 sudo apt install -y rofi
+echo "==> Installing network-manager-gnome"
 sudo apt-get install -y network-manager-gnome
+echo "==> Installing xdg-desktop-portal-wlr"
 sudo apt install -y xdg-desktop-portal-wlr
+echo "==> Installing mako-notifier"
 sudo apt install -y mako-notifier
 
 # pkg manager
+echo "==> Installing pipx"
 sudo apt install -y pipx
 
 # tools
+echo "==> Installing cmake"
 sudo apt install -y cmake
+echo "==> Installing ninja-build"
 sudo apt install -y ninja-build
+echo "==> Installing unzip"
+sudo apt install -y unzip
+echo "==> Installing brightnessctl"
+sudo apt install -y brightnessctl
+echo "==> Installing ddcutil"
+sudo apt install -y ddcutil
 
 # dotnet
+echo "==> Installing dotnet-sdk-10.0"
 sudo apt install -y dotnet-sdk-10.0
 
