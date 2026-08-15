2881411b3727122f3b0e9d0e4d3bb8207ebdd84f
Kristian JBF <10348902@pm.me>
2026-08-15 10:16:18 +0200

    feat(install): added k9s build from source to common-install.sh

--- install/arch-install.sh
+++ install/arch-install.sh
@@ -4,3 +4,6 @@ sudo pacman -S sway
 sudo pacman -S waybar
 sudo pacman -S rofi
 sudo pacman -S python-pipx
+sudo pacman -S mako-notifier
+sudo pacman -S cmake
+sudo pacman -S ninja-build
--- install/common-install.sh
+++ install/common-install.sh
@@ -34,7 +34,7 @@ fi
 
 
 # neovim (apt lags upstream — build a pinned stable release into ~/.local
-# so it never collides with the distro package; requires cmake + ninja-build,
+# requires cmake + ninja-build,
 # see ubuntu-install.sh / arch-install.sh)
 NVIM_VERSION="v0.12.4"
 NVIM_BIN="$HOME/.local/bin/nvim"
@@ -53,7 +53,6 @@ if [ "$NVIM_INSTALLED" != "$NVIM_VERSION" ]; then
     make -C "$SOURCE_DIR/neovim" CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="$HOME/.local"
     make -C "$SOURCE_DIR/neovim" install
 fi
-
 
 # zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
@@ -82,10 +81,29 @@ fi
 
 export PATH="$HOME/opt/go/bin:$HOME/go/bin:$PATH"
 
+# k9s (can be installed with home_brew, but i don't want 6000 package managers)
+K9S_VERSION="v0.51.0"
+K9S_BIN="$HOME/.local/bin/k9s"
+K9S_INSTALLED=""
+if [ -x "$K9S_BIN" ]; then
+    K9S_INSTALLED="$("$K9S_BIN" version --short | head -n1 | awk '{print $2}')"
+fi
+if [ "$K9S_INSTALLED" != "$K9S_VERSION" ]; then
+    if [ -d "$SOURCE_DIR/k9s" ]; then
+        git -C "$SOURCE_DIR/k9s" fetch --depth 1 origin tag "$K9S_VERSION"
+    else
+        git clone --depth 1 --branch "$K9S_VERSION" https://github.com/derailed/k9s.git "$SOURCE_DIR/k9s"
+    fi
+    git -C "$SOURCE_DIR/k9s" checkout "$K9S_VERSION"
+    make -C "$SOURCE_DIR/k9s" build
+    cp "$SOURCE_DIR/k9s/execs/k9s" "$K9S_BIN"
+fi
+
 # kubectl (official static binary — k8s.io/kubectl)
 KUBECTL_LATEST="$(curl -sSfL https://dl.k8s.io/release/stable.txt)"
 curl -sSfL "https://dl.k8s.io/release/${KUBECTL_LATEST}/bin/linux/${GO_ARCH}/kubectl" -o "$HOME/.local/bin/kubectl"
 chmod +x "$HOME/.local/bin/kubectl"
+
 
 # dendrite (markdown note engine)
 curl -fsSL https://raw.githubusercontent.com/KristianJBorgwarth/dendrite.daemon/master/install.sh | sh
