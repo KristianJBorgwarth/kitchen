4c70ba1b2ff744bf64ec13d1ada831174eedff3d
KristianJBorgwarth <10348902@pm.me>
2026-08-16 19:10:42 +0200

    fix: fix install scripts and add wl-clipboard

--- install/arch-install.sh
+++ install/arch-install.sh
@@ -3,18 +3,18 @@ PKGFILE="packages.txt"
 SOURCE_DIR="$HOME/source"
 mkdir -p "$SOURCE_DIR"
 
-while IFS= read -r pkg; do 
-  [ -z "$pkg" ] && continue # skip blank lines
-  case "$pkg" in \#*) continue ;; esac  # skip comment lines starting with # 
+sudo pacman -Sy
+
+while IFS= read -r pkg; do
+  [ -z "$pkg" ] && continue 
+  case "$pkg" in \#*) continue ;; esac
   echo "==> Installing $pkg"
-  sudo pacman -Sy "$pkg"
+  sudo pacman -S --needed --noconfirm "$pkg"
 done < "$PKGFILE"
 
 sudo pacman -S --needed base-devel
 sudo pacman -S openssh
 sudo pacman -S network-manager-applet
-
-# gtk/qt theming
 sudo pacman -S --needed papirus-icon-theme nwg-look qt5ct qt6ct
 
 if [ -d "$SOURCE_DIR/yay" ]; then
--- install/ubuntu-install.sh
+++ install/ubuntu-install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 PKGFILE="packages.txt"
 
-while IFS= read -r pkg; do 
-  [ -z "$pkg" ] && continue # skip blank lines
-  case "$pkg" in \#*) continue ;; esac  # skip comment lines starting with # 
+sudo apt-get update
+
+while IFS= read -r pkg; do
+  [ -z "$pkg" ] && continue
+  case "$pkg" in \#*) continue ;; esac
   echo "==> Installing $pkg"
-  sudo pacman -Sy "$pkg"
+  sudo apt-get install -y "$pkg"
 done < "$PKGFILE"
 
 sudo apt-get install -y network-manager-gnome
