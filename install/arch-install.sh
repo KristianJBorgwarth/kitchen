60255b31d3167dc9f055a612b5693357bab0805d
KristianJBorgwarth <10348902@pm.me>
2026-08-16 23:13:44 +0200

    fix: input for tilde no longer broken

--- install/arch-install.sh
+++ install/arch-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PKGFILE="packages.txt"
 SOURCE_DIR="$HOME/source"
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 mkdir -p "$SOURCE_DIR"
 
 sudo pacman -Sy
@@ -12,17 +13,25 @@ while IFS= read -r pkg; do
   sudo pacman -S --needed --noconfirm "$pkg"
 done < "$PKGFILE"
 
-sudo pacman -S --needed base-devel
-sudo pacman -S openssh
-sudo pacman -S network-manager-applet
-sudo pacman -S --needed papirus-icon-theme nwg-look qt5ct qt6ct
+sudo pacman -S --needed --noconfirm base-devel
+sudo pacman -S --needed --noconfirm openssh
+sudo pacman -S --needed --noconfirm network-manager-applet
+sudo pacman -S --needed --noconfirm papirus-icon-theme nwg-look qt5ct qt6ct
+sudo pacman -S --needed --noconfirm greetd
+
 
 if [ -d "$SOURCE_DIR/yay" ]; then
     git -C "$SOURCE_DIR/yay" pull
 else
     git clone https://aur.archlinux.org/yay.git "$SOURCE_DIR/yay"
 fi
-
 (cd "$SOURCE_DIR/yay" && makepkg -si --noconfirm)
 
-yay -Sy waypaper
+
+yay -Sy
+yay -S --needed --noconfirm waypaper greetd-tuigreet
+
+# login manager
+sudo systemctl enable greetd
+sudo mkdir -p /etc/greetd
+sudo ln -sf ${REPO_DIR}/greetd/config.toml /etc/greetd/config.toml
